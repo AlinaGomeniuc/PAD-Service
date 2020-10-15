561305d95db02ade468cdbf5943562d329781fb9
Alina Gomeniuc <agomeniuc@gmail.com>
2020-10-16 01:29:23 +0300

    added docker-compose

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,4 @@ WORKDIR /pad-service
 COPY . /pad-service/
 RUN pip3 install -r requirements.txt
 ENV PYTHONPATH .
-EXPOSE 5000
 CMD ["python3", "/pad-service//controller/user_controller.py"]
--- common/config.py
+++ common/config.py
@@ -3,7 +3,8 @@ from flask import Flask
 
 
 def configure_app():
-    connect("user-db")
+    connect(db="user-db",
+            host='mongodb://mongo_db')
     app = Flask(__name__)
     app.config['MONGODB_DB'] = 'user-db'
     return app
